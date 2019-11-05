f0914d4d7c23127e3449d6b35aa62e4e72546ad5
Matt Diamond <mattdiamond80@gmail.com>
2019-11-04 21:32:20 -0600

    Update sw_resgister

--- js/sw_register.js
+++ js/sw_register.js
@@ -9,11 +9,9 @@
 // }
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js", { scope: "/" })
-    .then(function(registration) {
-      console.log("Service Worker Registered");
-    });
+  navigator.serviceWorker.register("/sw.js").then(function(registration) {
+    console.log("Service Worker Registered");
+  });
 
   navigator.serviceWorker.ready.then(function(registration) {
     console.log("Service Worker Ready");
