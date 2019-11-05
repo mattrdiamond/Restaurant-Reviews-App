be3c851ec028ae3e8278663cfd60f9f6f3cc7246
Matt Diamond <mattdiamond80@gmail.com>
2019-11-04 21:13:49 -0600

    Update sw_register url for gh-pages

--- js/sw_register.js
+++ js/sw_register.js
@@ -8,14 +8,14 @@
 //   });
 // }
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js", { scope: "/" })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+    });
 
-if('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js', { scope: '/' })
-          .then(function(registration) {
-                console.log('Service Worker Registered');
-          });
-
-        navigator.serviceWorker.ready.then(function(registration) {
-           console.log('Service Worker Ready');
-        });
-      }
+  navigator.serviceWorker.ready.then(function(registration) {
+    console.log("Service Worker Ready");
+  });
+}
