19b0766bf91558d8154e5f87fc4270faf1b8acba
Matt Diamond <mattdiamond80@gmail.com>
2019-12-09 13:59:04 -0600

    Change responsive layouts for restaurant view

--- sw.js
+++ sw.js
@@ -30,72 +30,72 @@ const cacheFiles = [
 /**
  * Install service Worker
  */
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(function(cache) {
-        return cache.addAll(cacheFiles);
-      })
-      .then(() => self.skipWaiting())
-  );
-});
+// self.addEventListener("install", e => {
+//   e.waitUntil(
+//     caches
+//       .open(cacheName)
+//       .then(function(cache) {
+//         return cache.addAll(cacheFiles);
+//       })
+//       .then(() => self.skipWaiting())
+//   );
+// });
 
-/**
- * Activate service Worker
- */
-self.addEventListener("activate", e => {
-  console.log("Service Worker: Activated");
-  // Remove unwanted caches
-  e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== cacheName) {
-            console.log("Service Worker: Clearing Old Cache");
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
+// /**
+//  * Activate service Worker
+//  */
+// self.addEventListener("activate", e => {
+//   console.log("Service Worker: Activated");
+//   // Remove unwanted caches
+//   e.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.map(cache => {
+//           if (cache !== cacheName) {
+//             console.log("Service Worker: Clearing Old Cache");
+//             return caches.delete(cache);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
 
-/**
- * Call Fetch event to request resources from cache or network
- */
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    // 1. Check the cache first. (ignoreSearch will ignore the query string in the url)
-    caches.match(e.request, { ignoreSearch: true }).then(response => {
-      if (response) {
-        console.log("found in cache", e.request);
-        return response;
-      } else {
-        // 2. If request not in the cache, fetch from network
-        console.log("could not find in cache, FETCHING!", e.request);
-        return fetch(e.request)
-          .then(response => {
-            // 3. Ensure response is valid and that the request is from our origin
-            //    Prevents caching of every map tile that a user views
-            if (
-              !response ||
-              response.status !== 200 ||
-              response.type !== "basic"
-            ) {
-              return response;
-            }
-            // 4. Clone response and send the original to the browser and a copy to the cache
-            const responseClone = response.clone();
-            caches.open(cacheName).then(cache => {
-              cache.put(e.request, responseClone);
-            });
-            return response;
-          })
-          .catch(err => {
-            console.error(err);
-          });
-      }
-    })
-  );
-});
+// /**
+//  * Call Fetch event to request resources from cache or network
+//  */
+// self.addEventListener("fetch", function(e) {
+//   e.respondWith(
+//     // 1. Check the cache first. (ignoreSearch will ignore the query string in the url)
+//     caches.match(e.request, { ignoreSearch: true }).then(response => {
+//       if (response) {
+//         console.log("found in cache", e.request);
+//         return response;
+//       } else {
+//         // 2. If request not in the cache, fetch from network
+//         console.log("could not find in cache, FETCHING!", e.request);
+//         return fetch(e.request)
+//           .then(response => {
+//             // 3. Ensure response is valid and that the request is from our origin
+//             //    Prevents caching of every map tile that a user views
+//             if (
+//               !response ||
+//               response.status !== 200 ||
+//               response.type !== "basic"
+//             ) {
+//               return response;
+//             }
+//             // 4. Clone response and send the original to the browser and a copy to the cache
+//             const responseClone = response.clone();
+//             caches.open(cacheName).then(cache => {
+//               cache.put(e.request, responseClone);
+//             });
+//             return response;
+//           })
+//           .catch(err => {
+//             console.error(err);
+//           });
+//       }
+//     })
+//   );
+// });
